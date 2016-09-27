59034136a9aa3a7accf61920405db9ea692b92c1
Kate Norquay <katenorquay@live.com>
2016-09-27 14:56:20 +1300

    draft of quote generator

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,10 +1,5 @@
 function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
 
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-var currentQuote = '', currentAuthor = '';
-function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-}
 function getQuote() {
   $.ajax({
     headers: {
@@ -40,15 +35,6 @@ function getQuote() {
           }, 500);
           $('#author').html(r.author);
         });
-
-      var color = Math.floor(Math.random() * colors.length);
-      $("html body").animate({
-        backgroundColor: colors[color],
-        color: colors[color]
-      }, 1000);
-      $(".button").animate({
-        backgroundColor: colors[color]
-      }, 1000);
     }
   });
 }
@@ -58,11 +44,6 @@ $(document).ready(function() {
   $('#tweet-quote').on('click', function() {
     if(!inIframe()) {
       openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-    }
-  });
-  $('#tumblr-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
     }
   });
 });
